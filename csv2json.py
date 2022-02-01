ff6e20579c92a91824d3480132299078a37279a6
wyhallenwu <wyhforwork66@gmail.com>
2022-02-01 18:50:25 +0800

    update

--- csv2json.py
+++ csv2json.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 name_set = pd.read_csv('./dataset/name.csv')
 names = name_set['name']
+print(names)
+names.to_csv('./name.csv', index=0, header=0)
 for name in names:
     filename = './dataset/{name}.csv'.format(name=name)
     if os.path.exists(filename):
         data = pd.read_csv(filename)
         data = data.iloc[:, 1:]
-        print(data)
         data.to_json('./json_dataset/{name}.json'.format(name=name), orient='records')
